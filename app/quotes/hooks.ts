6379e8971f063e9c1f5812a26e9d58119e4df4c1
chanekx <mhechrist@gmail.com>
2025-08-09 23:11:34 +0800

    Change background image

--- app/quotes/hooks.ts
+++ app/quotes/hooks.ts
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { quoteApi, QuoteForFindOne } from "../service/quoteApiService";
 
 export const useHooks = () => {
   const [quote, setQuote] = useState<QuoteForFindOne>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchQuotes = async () => {
-    try{
+    try {
       const getQuote = await quoteApi.getOne();
-      console.log(getQuote)
-      setQuote(getQuote)
+      console.log(getQuote);
+      setQuote(getQuote);
+    } catch (e) {
+      console.log(e);
     }
-    catch(e){
-      console.log(e)
-    }
-  }
-  
+  };
 
-  return {quote, fetchQuotes};
+  const handleClick = async() => {
+    setIsLoading(true);
+    await fetchQuotes();
+    setTimeout(()=> setIsLoading(false),500)
+  }
+
+
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  return { quote, handleClick, isLoading };
 };
